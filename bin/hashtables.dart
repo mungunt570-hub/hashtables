e119723c2ec50ec6ca7b14f8c7d41af2602cbc07
mungunt570-hub <mungunt570@gmail.com>
2025-10-07 12:51:20 +0800

    prepare for hash search

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -3,7 +3,7 @@ import 'package:hashtables/linear_search.dart';
 
 void main() {
   final myList = [42, 3, 10, 7, 28, 149, 0, 3, 25, 7, 1];
-  final foundIndex = linearSearch(myList, 28);
+  var foundIndex = linearSearch(myList, 28);
   print(foundIndex);
 
   myList.sort();
