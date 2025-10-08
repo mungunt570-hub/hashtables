982ecfd35d6d2464616e08155410a1946fed5045
mungunt570-hub <mungunt570@gmail.com>
2025-10-09 06:34:42 +0800

    created an empty student list of size 3mil

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -1,4 +1,5 @@
 import 'package:hashtables/binary_search.dart';
+import 'package:hashtables/hash_search.dart';
 import 'package:hashtables/linear_search.dart';
 
 void main() {
@@ -7,8 +8,36 @@ void main() {
   print(foundIndex);
 
   myList.sort();
-  print(myList);
+  print(myList); // [0, 1, 3, 3, 7, 7, 10, 25, 28, 42, 149]
 
   foundIndex = binarySearch(myList, 1);
   print(foundIndex);
-}
+
+
+final studentList = List<Student?>.filled(3000000, null);
+//   final studentList = [
+//     Student(2024078, 'AZ1'),
+//     Student(2024077, 'AZ2'),
+//     Student(2021018, 'AD'),
+//     Student(2023139, 'BB'),
+//     Student(2025068, 'BS'),
+//     Student(2024008, 'DD'),
+//     Student(2024095, 'ES1'),
+//     Student(2024012, 'ED1'),
+//     Student(2024054, 'EB'),
+//     Student(2024081, 'ED2'),
+//     Student(2024111, 'ES2'),
+//     Student(2024059, 'MI'),
+//     Student(2024025, 'MB'),
+//     Student(2024020, 'MC'),
+//     Student(2023002, 'NM'),
+//     Student(2022081, 'TG'),
+//     Student(2024085, 'TK'),
+//     Student(2023096, 'TA'),
+//     Student(2022007, 'UM'),
+//     Student(2024007, 'VG'),
+//   ];
+// }
+
+final studentName = studentSearch(studentList, 2024008);
+print(studentName);
